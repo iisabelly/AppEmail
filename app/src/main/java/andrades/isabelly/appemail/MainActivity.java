24ca00d2822d6a8eb6f1d3e4041a5993bb2696d2
Isabelly <isabellyandrades.ifes@gmail.com>
2023-03-17 09:14:22 -0300

    Initial commit

---
+++ app/src/main/java/andrades/isabelly/appemail/MainActivity.java
@@ -0,0 +1,57 @@
+package andrades.isabelly.appemail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+
+        // Definicao da acao do click do botao
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtendo dados digitados pelo usuario
+                EditText etEmail = findViewById(R.id.etEmail);
+                EditText etAssunto = findViewById(R.id.etAssunto);
+                EditText etTexto = findViewById(R.id.etTexto);
+
+                String email = etEmail.getText().toString();
+                String assunto = etAssunto.getText().toString();
+                String texto = etTexto.getText().toString();
+
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+
+                i.setData(Uri.parse("mailto:"));
+
+                String[] emails = new String[]{email};
+                i.putExtra(Intent.EXTRA_EMAIL, emails);
+                i.putExtra(Intent.EXTRA_SUBJECT, assunto);
+                i.putExtra(Intent.EXTRA_TEXT, texto);
+
+                try {
+                    startActivity(Intent.createChooser(i, "Escolha o APP"));
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(MainActivity.this, "Não há nenhum app que posso " +
+                    "realizar essa operação", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+}
