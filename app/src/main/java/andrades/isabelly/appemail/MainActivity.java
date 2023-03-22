6873b8d4f7c8a6ef1556157db9306d6ff554ef6f
Isabelly <isabellyandrades.ifes@gmail.com>
2023-03-22 08:29:44 -0300

    Comentários

--- app/src/main/java/andrades/isabelly/appemail/MainActivity.java
+++ app/src/main/java/andrades/isabelly/appemail/MainActivity.java
@@ -20,30 +20,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Encontra o botão de envio na interface
         Button btnEnviar = findViewById(R.id.btnEnviar);
 
-        // Definicao da acao do click do botao
+        // Define a ação onclick do botão de envio
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Obtendo dados digitados pelo usuario
+                // Obtem os dados inseridos pelo usuário
                 EditText etEmail = findViewById(R.id.etEmail);
                 EditText etAssunto = findViewById(R.id.etAssunto);
                 EditText etTexto = findViewById(R.id.etTexto);
 
+                // Transforma os textos inseridos do editText em String
                 String email = etEmail.getText().toString();
                 String assunto = etAssunto.getText().toString();
                 String texto = etTexto.getText().toString();
 
+                // Cria uma intent implícita e define a sua action
                 Intent i = new Intent(Intent.ACTION_SENDTO);
-
                 i.setData(Uri.parse("mailto:"));
 
+                // Adiciona o conteúdo na intent
                 String[] emails = new String[]{email};
                 i.putExtra(Intent.EXTRA_EMAIL, emails);
                 i.putExtra(Intent.EXTRA_SUBJECT, assunto);
                 i.putExtra(Intent.EXTRA_TEXT, texto);
 
+                // Permite o usuário escolher o aplicativo de preferência para o envio do email e
+                // avisa caso não tenha nenhum aplicaivo que execute a action
                 try {
                     startActivity(Intent.createChooser(i, "Escolha o APP"));
                 } catch (ActivityNotFoundException e) {
